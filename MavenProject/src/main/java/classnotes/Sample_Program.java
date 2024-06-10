da062cf58e1e62a4474f43d85341e44cf6ff3b7c
iris <iris@QB-TP3-2282>
2024-06-10 21:09:59 +0530

    First program

---
+++ MavenProject/src/main/java/classnotes/Sample_Program.java
@@ -0,0 +1,13 @@
+package classnotes;
+
+public class Sample_Program {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		int sum=a+b;
+		System.out.println("The sum of two numbers:"+" "+sum);
+
+	}
+
+}
