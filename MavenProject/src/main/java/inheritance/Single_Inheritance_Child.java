91c8bb2af2bffb3c87020535565c5b25e7f586f5
iris <iris@QB-TP3-2282>
2024-06-17 17:29:55 +0530

    Inheritance_child

---
+++ MavenProject/src/main/java/inheritance/Single_Inheritance_Child.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Single_Inheritance_Child extends Single_Inhertiance_Parent{
+	public void print() {
+		System.out.println("Child");
+		}
+
+
+public static void main(String[] args) {
+	Single_Inheritance_Child obj=new Single_Inheritance_Child();
+	obj.print();
+	obj.display();
+}}
+	
